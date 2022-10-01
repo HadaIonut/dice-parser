d95c525a95045690348c3b53d6b9d6b5db6df2f7
bmarian <33843056+bmarian@users.noreply.github.com>
2022-10-01 18:47:49 +0300

    ParseSync

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import {getCacheMap} from './parser/ParserCommon'
 import {getDiceRollAsync} from './parser/ParserAsync'
+import {getDiceRollSync} from './parser/ParserSync'
 
 (async () => {
   console.log(getCacheMap().get('20'))
-  console.log((await getDiceRollAsync(20)))
+  console.log((await getDiceRollSync(20)))
   console.log(getCacheMap().get('20'))
-  console.log((await getDiceRollAsync(20)))
+  console.log((await getDiceRollSync(20)))
   console.log(getCacheMap().get('20'))
-  console.log((await getDiceRollAsync(20)))
+  console.log((await getDiceRollSync(20)))
   console.log(getCacheMap().get('20'))
 })()
 
--- src/parser/ParserSync.ts
+++ src/parser/ParserSync.ts
@@ -1,1 +1,12 @@
-export { }
+import {hasCache, getFromCache, addToCache} from './ParserCommon'
+import {getRandomIntegers} from '../random-numbers/JavascriptRandom'
+
+export const getDiceRollSync = (dice: number): number => {
+  const id = `${dice}`
+
+  const isCached: boolean = hasCache(id)
+  if (isCached) return getFromCache(id) as number
+
+  const generatedValues: number[] = getRandomIntegers({max: dice})
+  return addToCache(id, generatedValues) as number
+}
